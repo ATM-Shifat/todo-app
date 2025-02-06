f4e2bf895107b32c71f86526d1c1c35b3e3be50a
atm-shfiat <shifatatm@gmail.com>
2025-02-06 21:20:21 +0600

    feat(auth): implement login method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -13,13 +13,9 @@ router.post("/register", (req, res) => {
     if(!username ||!password) return res.status(400).send("Username and password are required")
 
     //check if user exists
-    // const user = db.query("SELECT * FROM USERS WHERE username = ?", [username])
-
-    // console.log(user)
-
-    //if(user) return res.status(400).send("Already an user")
-
-
+    const getUser = db.prepare("SELECT COUNT(*) as count FROM USERS WHERE username = ?")
+    const user = getUser.get(username)
+    if(user.count > 0) return res.status(400).send({ message:"Username already exists"})
 
     const hashedPassword = bcrypt.hashSync(password, salt)
 
@@ -37,25 +33,48 @@ router.post("/register", (req, res) => {
 
         const token = jwt.sign(
             {id: result.lastInsertRowid}, 
-            process.env.JWT_SECRETE, 
+            process.env.JWT_SECRET, 
             {expiresIn: "24h"}
         )
-
-
-        // res.json({token})
-        // res.status(201)
 
         res.status(201).send({token})
 
     }catch(error){
-        console.error(error)
+        console.error(error.message)
         return res.sendStatus(503)
     }
 
 })
 
 router.post("/login", (req, res) => {
-    res.sendStatus(200)
+    const {username, password} = req.body
+
+    try{
+        //Check if the user exists
+        //const getUser = db.prepare("SELECT EXISTS (SELECT 1 FROM USERS WHERE username = ?) AS userCount")
+        //const getUser = db.prepare(`SELECT COUNT(*) AS count FROM USERS WHERE username = ?`)
+        //const getUser = db.prepare("SELECT id password FROM USERS WHERE username = ?")
+ 
+        const getUser = db.prepare(`SELECT id, password FROM USERS WHERE username = ?`) 
+        const user = getUser.get(username)
+        if(!user) return res.status(404).send({message: "User not found!"})
+        
+        //Check password
+        const validPassword = bcrypt.compareSync(password, user?.password)
+        if(!validPassword) return res.status(401).send({message: "Invalid email or password!"})
+        
+        const token = jwt.sign(
+            {id: user?.id},
+            process.env.JWT_SECRET, 
+            {expiresIn: "24h"}
+        )
+
+        res.status(200).send({token: token})
+
+    }catch(error){
+        console.error(error.message)
+        return res.sendStatus(503)
+    }
 })
 
 export default router
