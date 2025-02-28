80fef483e79fcbd7a0c62db40e32de473f8040a4
atm-shfiat <shifatatm@gmail.com>
2025-02-28 16:33:57 +0600

    Revert "Merge pull request #1 from ATM-Shifat/postgresql"
    This reverts commit dec9a6658774faa75e8fb97158c6a71a5bd500e7, reversing
    changes made to c06020dc1dcf4a185f79d6e4f33ef187a2567c21.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,51 +1,38 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import prisma from '../prismaClient.js';
+import db from '../db.js';
 
 const router = express.Router()
 const salt = Number(process.env.SALT)
 
 //Register Users
-router.post("/register", async(req, res) => {
+router.post("/register", (req, res) => {
     const {username, password} = req.body
 
-    //Checking provided information
     if(!username ||!password) return res.status(400).send("Username and password are required")
 
     //check if user exists
-    const user = await prisma.user.findUnique({
-        where: {
-            username: username
-        }
-    })
-    if(user) return res.status(409).send({message: "User already exists!"})
+    const getUser = db.prepare("SELECT COUNT(*) as count FROM USERS WHERE username = ?")
+    const user = getUser.get(username)
+    if(user.count > 0) return res.status(400).send({ message:"Username already exists"})
 
     const hashedPassword = bcrypt.hashSync(password, salt)
 
     try{
 
         //create user
-        const user = await prisma.user.create({
-            data: {
-                username,
-                password: hashedPassword,
-            },
-        })
+        const insertUser = db.prepare(`INSERT INTO USERS (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
 
         const defaultTodo = `Hello :) first todo`
 
-        //Insert default task
-        await prisma.todo.create({
-            data: {
-                userId: user.id,
-                task: defaultTodo,
-            }
-        })
-        
-        //Generation of JWT token
+        //create default task
+        const insertTodo = db.prepare(`INSERT INTO TODOS (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
         const token = jwt.sign(
-            {id: user.id}, 
+            {id: result.lastInsertRowid}, 
             process.env.JWT_SECRET, 
             {expiresIn: "24h"}
         )
@@ -54,25 +41,22 @@ router.post("/register", async(req, res) => {
 
     }catch(error){
         console.error(error.message)
-        return res.status(500).json({message: "Server error"})
+        return res.sendStatus(503)
     }
 
 })
 
-router.post("/login", async (req, res) => {
+router.post("/login", (req, res) => {
     const {username, password} = req.body
 
-    //Checking provided information
-    if(!username ||!password) return res.status(400).send("Username and password are required")
-
     try{
-
         //Check if the user exists
-        const user = await prisma.user.findUnique({
-            where: {
-                username: username
-            }
-        })
+        //const getUser = db.prepare("SELECT EXISTS (SELECT 1 FROM USERS WHERE username = ?) AS userCount")
+        //const getUser = db.prepare(`SELECT COUNT(*) AS count FROM USERS WHERE username = ?`)
+        //const getUser = db.prepare("SELECT id password FROM USERS WHERE username = ?")
+ 
+        const getUser = db.prepare(`SELECT id, password FROM USERS WHERE username = ?`) 
+        const user = getUser.get(username)
         if(!user) return res.status(404).send({message: "User not found!"})
         
         //Check password
@@ -89,7 +73,7 @@ router.post("/login", async (req, res) => {
 
     }catch(error){
         console.error(error.message)
-        return res.status(500).json({message: "Server error"})
+        return res.sendStatus(503)
     }
 })
 
